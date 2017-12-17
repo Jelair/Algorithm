75a22d12605d812d5c24a558407c78ed742cae8f
simplefly <1059229782@qq.com>
2017-12-18 02:54:01 +0800

    algorithm

---
+++ src/com/simplefly/algorithm/Solution_9.java
@@ -0,0 +1,24 @@
+/**  
+ * @Title:  Solution_9.java   
+ * @Package com.simplefly.algorithm   
+ * @Description:    TODO(用一句话描述该文件做什么)   
+ * @author: simplyfly   
+ * @date:   2017年12月18日 上午2:41:42   
+ * @version V1.0 
+ * @Copyright: 2017 SimplyFly2016 All rights reserved. 
+ */  
+package com.simplefly.algorithm;
+
+/**   
+ * @ClassName:  Solution_9   
+ * @Description:一只青蛙一次可以跳上1级台阶，也可以跳上2级……它也可以跳上n级。求该青蛙跳上一个n级的台阶总共有多少种跳法。 
+ * @author: simplyfly 
+ * @date:   2017年12月18日 上午2:41:42   
+ *     
+ * @Copyright: 2017 SimplyFly2016 All rights reserved. 
+ */
+public class Solution_9 {
+    public int JumpFloorII(int target) {
+        return 0;
+    }
+}
